966c7671d25a27fcbf8ff91429a379dd477b3c0f
Arthur <arthur.martins@hbsis.com.br>
2020-04-20 10:09:46 -0300

    base line classifier

---
+++ 06_rules_Orange_census.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Editor Spyder
+
+by Claudiney 2020.04.20  10:41 hs
+"""
+import Orange
+
+base = Orange.data.Table('census.csv')
+#    na base de dados c# = classificador, i# = ignorar
+base.domain
+
+# dividir base de dados em base treinamento(75%) e teste(25%)
+base_dividida = Orange.evaluation.testing.sample(base, n=0.25)
+base_treinamento = base_dividida[1]
+base_teste = base_dividida[0]
+len(base_treinamento)
+len(base_teste)
+
+#    algoritmo antigo para indução de regras
+cn2_learner = Orange.classification.rules.CN2Learner()
+#    Learner é a variável que gera as regras e as REGRAS são os classificadores
+classificador = cn2_learner(base_treinamento)
+#    vendo as regras criadas
+for regras in classificador.rule_list:
+    print(regras)
+    
+#    teste:
+resultado = Orange.evaluation.testing.TestOnTestData(base_treinamento, base_teste, [classificador])
+print(Orange.evaluation.CA(resultado))  # CA = classification accuracy
---
+++ 07_majority_learner_cv.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 09:22:39 2020
+
+@author: USUARIO-PC
+"""
+import Orange
+
+base = Orange.data.Table('credit_data.csv')
+base.domain
+
+# dividir base de dados em base treinamento(75%) e teste(25%)
+base_dividida = Orange.evaluation.testing.sample(base, n=0.25)
+base_treinamento = base_dividida[1]
+base_teste = base_dividida[0]
+len(base_treinamento)
+len(base_teste)
+
+classificador = Orange.classification.MajorityLearny()
+resultado = Orange.evaluation.testing.TestOnTestData(base_treinamento, base_teste, [classificador])
+print(Orange.evaluation.CA(resultado))
