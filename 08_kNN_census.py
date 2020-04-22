a2747283b48912560b8c2bd604f3ac721bdade45
Arthur <arthur.martins@hbsis.com.br>
2020-04-22 10:55:59 -0300

    class 67

--- 08_kNN_census.py
+++ 08_kNN_census.py
@@ -6,48 +6,37 @@ Created on Tue Apr 21 10:40:09 2020
 """
 
 import pandas as pd
+ 
+base = pd.read_csv('census.csv')
+ 
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+                
+from sklearn.preprocessing import LabelEncoder ,OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ 
+onehotencorder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
+previsores = onehotencorder.fit_transform(previsores).toarray()
+ 
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
 
-basec = pd.read_csv('census.csv')
-
-predictors = basec.iloc[:, 0:14].values
-classifier = basec.iloc[:, 14].values
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_predictors = LabelEncoder()
-
-predictors[:, 1] = labelencoder_predictors.fit_transform(predictors[:, 1])
-predictors[:, 3] = labelencoder_predictors.fit_transform(predictors[:, 3])
-predictors[:, 5] = labelencoder_predictors.fit_transform(predictors[:, 5])
-predictors[:, 6] = labelencoder_predictors.fit_transform(predictors[:, 6])
-predictors[:, 7] = labelencoder_predictors.fit_transform(predictors[:, 7])
-predictors[:, 8] = labelencoder_predictors.fit_transform(predictors[:, 8])
-predictors[:, 9] = labelencoder_predictors.fit_transform(predictors[:, 9])
-predictors[:, 13] = labelencoder_predictors.fit_transform(predictors[:, 13])
-
-# ONE HOT ENCODER
-
-one_hot = OneHotEncoder(categories='auto')
-predictors = one_hot.fit_transform(predictors).toarray()
-
-labelencoder_class = LabelEncoder()
-classifier = labelencoder_class.fit_transform(classifier)
-
-# ESCALONAMENTO
+# ESCALONAMENTO 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-predictors = scaler.fit_transform(predictors)
-
-# DIVISÃO DE BASES
-from sklearn.cross_validation import train_test_split
-train_predictors, test_predictors, train_class, test_class = train_test_split(predictors, classifier, test_size=0.15, random_state=0)
-
-# importação biblioteca
-# criação do classificador
-
-classificador.fit(train_predictors, train_class)
-predicts = classificador.predict(test_predictors)
-
-# comparação de previsão com o real
+previsores = scaler.fit_transform(previsores)
+ 
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+ 
+from sklearn.neighbors import KNeighborsClassifier
+classificador = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+ 
 from sklearn.metrics import confusion_matrix, accuracy_score
-precision = accuracy_score(test_class, predicts)
-matrix = confusion_matrix(test_class, predicts)
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+ 
+import collections
+collections.Counter(classe_teste)
